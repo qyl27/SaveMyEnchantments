7af049c9a6758c43de97096f6e6472ae3d9602f3
秋雨落 <i@rain.cx>
2021-05-13 13:21:58 +0800

    :sparkles: 也许能用了
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/savemyenchantments/listener/ListenerInventoryClick.java
+++ src/main/java/cx/rain/mc/savemyenchantments/listener/ListenerInventoryClick.java
@@ -1,30 +1,54 @@
 package cx.rain.mc.savemyenchantments.listener;
 
+import cx.rain.mc.savemyenchantments.utility.GrindstoneHelper;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.GrindstoneInventory;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class ListenerInventoryClick implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getClickedInventory() == null) {
+        Inventory inv = event.getClickedInventory();
+
+        if (inv == null) {
             return;
         }
         
-        if (event.getClickedInventory().getType() != InventoryType.GRINDSTONE) {
+        if (event.getView().getType() != InventoryType.GRINDSTONE) {
             return;
         }
 
-//        if (!(event.getClickedInventory() instanceof GrindstoneInventory)) {
-//            return;
-//        }
+        if (!(inv instanceof GrindstoneInventory)) {
+            return;
+        }
+
+        if (!event.getWhoClicked().hasPermission("savemyenchantments.use")) {
+            return;
+        }
 
         GrindstoneInventory grindstone = (GrindstoneInventory) event.getClickedInventory();
-        ItemStack inputUp = grindstone.getItem(0);
-        ItemStack inputDown = grindstone.getItem(1);
 
+        ItemStack cursor = event.getCursor();
+        if (cursor != null) {
+            if (cursor.getType() == Material.BOOK) {
+                if (event.getRawSlot() == 1) {
+                    event.setCancelled(true);
+
+                    event.setCurrentItem(cursor);
+                    event.setCursor(null);
+
+                    GrindstoneHelper.update(grindstone);
+                }
+            }
+        } else {
+            if (event.getRawSlot() == 2) {
+                event.setCursor(GrindstoneHelper.doDisenchantment(grindstone));
+            }
+        }
     }
 }
--- src/main/java/cx/rain/mc/savemyenchantments/utility/GrindstoneHelper.java
+++ src/main/java/cx/rain/mc/savemyenchantments/utility/GrindstoneHelper.java
@@ -1,27 +1,95 @@
 package cx.rain.mc.savemyenchantments.utility;
 
+import de.tr7zw.changeme.nbtapi.*;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.GrindstoneInventory;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Map;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 public class GrindstoneHelper {
     public static boolean canDisenchant(ItemStack stack) {
         return !stack.getEnchantments().isEmpty();
     }
 
-    public static Enchantment disenchant(ItemStack stack) {
-        if (canDisenchant(stack)) {
-            Map<Enchantment, Integer> enchantments = stack.getEnchantments();
+    public static ItemStack getEnchantBook(Tuple<Enchantment, Integer> enchantment) {
+        ItemStack stack = new ItemStack(Material.ENCHANTED_BOOK);
+        EnchantmentStorageMeta meta = (EnchantmentStorageMeta) stack.getItemMeta();
+        assert meta != null;
+        meta.addStoredEnchant(enchantment.left, enchantment.right, true);
+        stack.setItemMeta(meta);
+        return stack;
+    }
 
+    public static Tuple<ItemStack, ItemStack> disenchant(ItemStack stack) {
+        if (canDisenchant(stack)) {
+            NBTItem nbt = new NBTItem(stack);
+            NBTCompound tag = nbt.getCompound("tag");
+            NBTCompoundList enchantments = tag.getCompoundList("Enchantments");
+
+            int size = enchantments.size();
+            assert !enchantments.isEmpty();
+
+            NBTListCompound enchantment = enchantments.get(size - 1);
+            String id = enchantment.getString("id");
+            String[] key = id.split(":");
+            int lvl = enchantment.getInteger("lvl");
+            ItemStack book = getEnchantBook(new Tuple<>(
+                    Enchantment.getByKey(new NamespacedKey(key[0], key[1])), lvl));
+
+            enchantments.remove(size - 1);
+            tag.setObject("Enchantments", enchantments);
+            nbt.setObject("tag", tag);
+            ItemStack item = nbt.getItem();
+
+            return new Tuple<>(item, book);
         } else {
             return null;
         }
     }
 
-    public static ItemStack getEnchantBook(ItemStack stack) {
-        if (canDisenchant(stack)) {
+    public static ItemStack tryDisenchant(ItemStack stack) {
+        Tuple<ItemStack, ItemStack> stackTuple = disenchant(stack);
+        if (stackTuple != null) {
+            return stackTuple.left;
         }
+
+        return null;
+    }
+
+    public static void update(GrindstoneInventory inventory) {
+        ItemStack up = inventory.getItem(0);
+        ItemStack down = inventory.getItem(1);
+
+        if (up == null
+                || down == null) {
+            return;
+        }
+
+        if (canDisenchant(up)) {
+            inventory.setItem(2, tryDisenchant(up));
+        }
+    }
+
+    public static ItemStack doDisenchantment(GrindstoneInventory inventory) {
+        ItemStack up = inventory.getItem(0);
+        ItemStack down = inventory.getItem(1);
+
+        if (up == null
+                || down == null) {
+            return null;
+        }
+
+        if (canDisenchant(up)) {
+            Tuple<ItemStack, ItemStack> stackTuple = disenchant(up);
+
+            if (stackTuple != null) {
+                inventory.setItem(1, stackTuple.right);
+                return stackTuple.left;
+            }
+        }
+
         return null;
     }
 }
